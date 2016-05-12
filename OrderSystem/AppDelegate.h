31f9b89aa8fb92c841d50831170b5e4284b125f5
fengwujie <allen@allendeMac.local>
2016-05-12 23:56:21 +0800

    create
    create

---
+++ OrderSystem/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OrderSystem
+//
+//  Created by allen on 16/5/12.
+//  Copyright © 2016年 allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OrderSystem/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OrderSystem
+//
+//  Created by allen on 16/5/12.
+//  Copyright © 2016年 allen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
